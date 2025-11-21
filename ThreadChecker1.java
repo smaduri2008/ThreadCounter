9ae3161a6cf225bcebb871770522cd2af1c12a95
sahas <sahas.maduri@gmail.com>
2025-11-21 13:48:07 -0500

    created threads array

--- ThreadChecker1.java
+++ ThreadChecker1.java
@@ -3,23 +3,5 @@ public class ThreadChecker1
 	public static void main(String []args)
 	{
 		Thread[] threads = new Thread[1000];
-
-		Runnable counter = () -> {
-		    long sum = 0;
-		    for (int i = 1; i <= 1_000_000; i++) {
-		        sum += i;
-		    }
-		};
-
-		long[] results = new long[1000];
-
-		for (Thread t : threads) t.start();
-		for (Thread t : threads) {
-		    try {
-		        t.join();
-		    } catch (InterruptedException e) {
-		        e.printStackTrace();
-		    }
-}
 	}
 }
