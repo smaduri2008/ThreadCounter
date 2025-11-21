636a74e23721dd8f493ff6f230ef0c2cb847a44d
sahas <sahas.maduri@gmail.com>
2025-11-21 13:51:21 -0500

    finished code

--- ThreadChecker1.java
+++ ThreadChecker1.java
@@ -1,25 +1,41 @@
 public class ThreadChecker1
 {
-	public static void main(String []args)
-	{
-		Thread[] threads = new Thread[1000];
+    public static void main(String[] args)
+    {
+        Thread[] threads = new Thread[1000];
 
-		Runnable counter = () -> {
-		    long sum = 0;
-		    for (int i = 1; i <= 1_000_000; i++) {
-		        sum += i;
-		    }
-		};
+        long[] results = new long[1000];
 
-		long[] results = new long[1000];
+        for (int i = 0; i < threads.length; i++) {
 
-		for (Thread t : threads) t.start();
-		for (Thread t : threads) {
-		    try {
-		        t.join();
-		    } catch (InterruptedException e) {
-		        e.printStackTrace();
-		    }
-		}
-	}
+            final int index = i;
+
+            threads[i] = new Thread(() -> {
+                long sum = 0;
+                for (int n = 1; n <= 1_000_000; n++) {
+                    sum += n;
+                }
+                results[index] = sum;
+            });
+        }
+
+        for (Thread t : threads) {
+            t.start();
+        }
+
+        for (Thread t : threads) {
+            try {
+                t.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        long finalTotal = 0;
+        for (long r : results) {
+            finalTotal += r;
+        }
+
+        System.out.println("Final total: " + finalTotal);
+    }
 }
