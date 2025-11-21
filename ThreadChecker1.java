6e40ed76bcc2bffdf5c847a5aefc33b1511f0793
sahas <sahas.maduri@gmail.com>
2025-11-21 13:48:57 -0500

    created runnable to add sum

--- ThreadChecker1.java
+++ ThreadChecker1.java
@@ -3,5 +3,14 @@ public class ThreadChecker1
 	public static void main(String []args)
 	{
 		Thread[] threads = new Thread[1000];
+
+		Runnable counter = () -> {
+		    long sum = 0;
+		    for (int i = 1; i <= 1_000_000; i++) {
+		        sum += i;
+		    }
+		};
+
+
 	}
 }
